e333891bd81eaa15c71c9ff02946af08e2b0eff0
Anya Naumchik <anyanaumchik16@gmail.com>
2020-05-13 01:11:00 +0300

    Initial commit

---
+++ src/main/java/bsuir/naumchik/Application.java
@@ -0,0 +1,13 @@
+package bsuir.naumchik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
